1a601534a829b12402290b35433195b480a7ccf8
Rogelio Servin <mancahster1509@gmail.com>
2025-02-02 12:07:50 -0600

    image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import crisImg from "../public/images/cristina.jpg";
 
 function App() {
-  return <>Cristina</>;
+  return (
+    <div>
+      <h1>Cristina❤️</h1>
+      <img src={crisImg} alt="Cris" />
+    </div>
+  );
 }
 
 export default App;
